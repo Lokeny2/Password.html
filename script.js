4e17bb52cc5705bfe8152be1b0a5cc969bed18c3
Lokeny2 <lokenymoses2@gmail.com>
2026-03-12 11:32:35 +0300

    Updated script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+    // 1. Get the elements from the DOM
+const passwordInput = document.getElementById('password');
+const strengthSpan = document.getElementById('Strength');
+
+// 2. Adding the event listener
+passwordInput.addEventListener('input', function() {
+    const password = passwordInput.value;
+    let strength = 'Weak'; 
+    let messageColor = 'red'; 
+
+    // factors used for checking strength
+    if (password.length === 0) {
+        strength = 'Waiting...';
+        messageColor = 'gray';
+    } 
+    else if (password.length < 6) {
+        strength = 'Very Weak';
+        messageColor = 'darkred';
+    } 
+    else if (password.length < 8) {
+        strength = 'Weak';
+        messageColor = 'red';
+    } 
+    else {
+        // where password is 8+ characters, we check complexity.
+        const hasLetters = /[A-Za-z]/.test(password);
+        const hasNumbers = /[0-9]/.test(password);
+        const hasSpecialCharacters = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+        if (hasLetters && hasNumbers && hasSpecial && password.length >= 10) {
+            strength = 'Very Strong';
+            messageColor = 'green';
